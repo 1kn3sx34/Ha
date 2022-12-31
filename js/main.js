665f214887892da1bd877d4b683a00943cbae59a
1kn3sx34-10 <98346220+1kn3sx34@users.noreply.github.com>
2022-12-30 19:30:17 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let array_script=[];
+let count= 0;
+let text= document.querySelector("#text");
+let container= document.querySelector("#container");
+
+for(h of hacking_script){
+  array_script.push(h);
+}
+
+console.log(array_script);
+
+// window.addEventListener("keypress",()=>{
+  
+// })
