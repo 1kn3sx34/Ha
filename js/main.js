a89b9cb55728cd4e8d97e657f6b061ecbc652254
1kn3sx34-10 <98346220+1kn3sx34@users.noreply.github.com>
2022-12-31 09:35:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@ for(h of hacking_script){
 console.log(array_script);
 
 window.addEventListener("keypress",()=>{
+  // auto scroll
+  container.scrollTop = container.scrollHeight;
   count++;
   if((array_script[count-1]=="{" || array_script[count-1] == "}" || array_script[count-1] == ";" || array_script[count-1] == ")" || array_script[count-1] == "/") && array_script[count] == " "){
     text.innerHTML += array_script[count-1] + '<br><br>';
-    array_script[count]
+    array_script[count] = '';
+  }else{
+    text.innerHTML += array_script[count-1]
   }
-})
+});
